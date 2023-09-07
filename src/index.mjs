193d32cae2371c6c810ff4192f53d84164c65ec9
Reiji Kono <94098626+kono-meowmeow@users.noreply.github.com>
2023-09-07 14:28:02 +0000

    変数の宣言

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,65 @@
-import "./styles.css";
+// const, let 等の変数宣言
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+var val1 = "var変数";
+console.log(val1);
+
+// var変数は上書き可能
+val1 = "var変数を上書き";
+console.log(val1);
+
+// var変数は再宣言可能
+var val1 = "var変数を再宣言";
+console.log(val1);
+
+let val2 = "let変数";
+console.log(val2);
+
+// letは上書きが可能
+val2 = "let変数を上書き";
+console.log(val2);
+
+// letは再宣言不可能
+// let val2 = "let変数を再宣言";
+// Uncaught SyntaxError: Identifier 'val2' has already been declared
+
+const val3 = "const変数";
+console.log(val3);
+
+// const変数は上書き不可能
+// val3 = "const変数を上書き";
+// Uncaught TypeError: Assignment to constant variable.
+
+// const変数は再宣言不可能
+// const val3 = "const変数を再宣言";
+// Uncaught SyntaxError: Identifier 'val3' has already been declared
+
+// constでオブジェクトを宣言する
+const val4 = {
+  name: "Tom",
+  age: 3
+};
+console.log(val4);
+
+// constで宣言したオブジェクトのプロパティを変えることができる
+val4.name = "Jerry";
+console.log(val4);
+
+// constで宣言したオブジェクトに、新しいプロパティを追加することもできる
+val4.address = "NY";
+console.log(val4);
+
+// constで配列を宣言する
+const val5 = ['dog', 'cat'];
+console.log(val5);
+
+// constで宣言した配列のプロパティを変更することができる
+val5[0] = "bird";
+console.log(val5);
+
+// constで宣言した配列に、新しいプロパティを追加することもできる
+val5.push("meow");
+console.log(val5);
+
+// このように、オブジェクトや配列はconstで宣言しても変更ができる
+// よって、Reactではほとんどをconstで宣言することになる
+// 処理の中で上書きが必要な変数にはletを使う
